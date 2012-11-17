156a60866f0fbfd48f28ff698b7a9d639439555c
Sean Clark Hess <seanhess@gmail.com>
2012-11-17 14:31:38 -0700

    finished SharedObject

--- public/controllers/Test.ts
+++ public/controllers/Test.ts
@@ -1,15 +1,42 @@
 ///<reference path="../def/angular.d.ts"/>
 ///<reference path="../services/SharedObject.ts"/>
 ///<reference path="../services/FB.ts"/>
+///<reference path="../services/Id.ts"/>
 
 angular.module('controllers')
-.controller('TestCtrl', function($scope, SharedObject:SharedObjectService, FB:IFirebaseService) {
+.controller('TestCtrl', function($scope, SharedObject:SharedObjectService, FB:IFirebaseService, Id:IdService) {
   $scope.message = "hello"
-  var testRef = FB.ref("/test/fake2")
-  var so = SharedObject.object(testRef)
-  $scope.object = so.value
 
-  $scope.save = function() {
-    SharedObject.set(so) 
+  //var testRef = FB.ref("/test/fake2")
+  //var so = SharedObject.object(testRef)
+  //$scope.person = so.value
+
+  var sa = SharedObject.bindArray(FB.ref("/test/people"))
+  $scope.people = sa.value
+
+  $scope.addPerson = function() {
+    SharedObject.add(sa.ref, {id:Id.randomId()})
   }
+
+  $scope.remove = function(person) {
+    SharedObject.remove(sa.ref, person)
+  }
+
+  $scope.select = function(person) {
+    $scope.person = person
+  }
+
+  $scope.save = function(person) {
+    SharedObject.setChild(sa.ref, person)
+  }
+
+  // Well, when it comes to the array, like the players, I just want them to all update magically.
+  // without me having to rebind to any of them.
+  // yeah.
+
+  // but I DO want to be able to UPDATE them
+  // you need a standard id system. So you can just say: shared array, update this object
+  // and it will do so based on ITS saved ref
+  // I don't need to access the individual refs
+
 })
--- public/services/SharedObject.ts
+++ public/services/SharedObject.ts
@@ -4,72 +4,217 @@
 ///<reference path="./FB"/>
 ///<reference path="../def/underscore.d.ts"/>
 
-interface SharedArray {
-  ref: fire.IRef;
-  value:any[];
-}
-
 interface SharedObject {
   ref: fire.IRef;
   value:any;
   updating?:bool;
+  onValue?(snap:fire.ISnapshot);
+}
+
+interface SharedArrayItem {
+  id:string;
+}
+
+interface SharedArray extends SharedObject {
+  value:SharedArrayItem[];
+  onAdded?(snap:fire.ISnapshot);
+  onChanged?(snap:fire.ISnapshot);
+  onRemoved?(snap:fire.ISnapshot);
 }
 
 interface SharedObjectService {
-  object(ref:fire.IRef):SharedObject;
-  set(so:SharedObject);
-  update(so:SharedObject, property:string);
+  bindObject(ref:fire.IRef):SharedObject;
+  unbindObject(so:SharedObject);
+  set(ref:fire.IRef, value:any);
+  update(ref:fire.IRef, value:any, property:string);
+
+  bindArray(ref:fire.IRef):SharedArray;
+  unbindArray(so:SharedArray);
+
+  add(arrayRef:fire.IRef, item:SharedArrayItem);
+  remove(arrayRef:fire.IRef, item:SharedArrayItem);
+  setChild(arrayRef:fire.IRef, item:SharedArrayItem);
 }
 
 angular.module('services')
 .factory('SharedObject', function($rootScope:ng.IScope, FB:IFirebaseService):SharedObjectService {
 
+  // whether we are currently responsible for an update and should ignore any changes to the remote object
+  var updating = false
+
   return {
-    object:object,
+    bindObject:bindObject,
+    unbindObject:unbindObject,
     update:update,
     set:set,
+
+    bindArray:bindArray,
+    unbindArray:unbindArray,
+    add: add,
+    remove: remove,
+    setChild: setChild,
   }
 
   // these connect them up
-  function object(ref:fire.IRef):SharedObject {
+  function bindObject(ref:fire.IRef):SharedObject {
     var so:SharedObject = {ref: ref, value:{}}
 
-    // if null, then delete all properties
-    ref.on('value', function(snapshot:fire.ISnapshot) {
-
-      // ignore if we caused the value change
-      if (so.updating) return
-
-      $rootScope.$apply(function() {
-        var value = snapshot.val()
-        if (!value) return
-        _.extend(so.value, value)
-      })
+    so.onValue = makeUpdate(function(value) {
+      if (!value) return objectEmpty(so.value)
+      _.extend(so.value, value)
     })
 
+    // if null, then delete all properties
+    ref.on('value', so.onValue)
     return so
   }
 
-  // only work on shared objects! not shared arrays!
-  function set(so:SharedObject) {
-    var updates = {}
-    var obj = so.value
-    Object.keys(obj).forEach(function(key) {
-      if (obj[key] && !key.match(/^\$/))
-        updates[key] = obj[key]
-    })
-
-    // ignore changes between these!
-    so.updating = true
-    so.ref.set(updates)
-    so.updating = false
+  function unbindObject(so:SharedObject) {
+    so.ref.off('value', so.onValue)
   }
 
-  function update(so:SharedObject, property:string) {
-    so.ref.child(property).set(so[property])
+  // only work on shared objects! not shared arrays!
+  function set(ref:fire.IRef, object:any, shouldIgnoreServer?:bool = true) {
+    var updates = {}
+    Object.keys(object).forEach(function(key) {
+      if (object[key] && !key.match(/^\$/))
+        updates[key] = object[key]
+    })
+
+    if (!shouldIgnoreServer) 
+      return ref.set(updates)
+
+    ignoreServer(function() {
+      ref.set(updates)
+    })
+  }
+
+  function update(ref:fire.IRef, object:any, property:string) {
+    ref.child(property).set(object[property])
+  }
+
+
+  function makeUpdate(f:fire.IValueCB):fire.ISnapshotCB {
+    return function(snap:fire.ISnapshot) {
+      if (updating) return
+
+      if ((<any>$rootScope).$$phase)
+        return f(snap.val())
+
+      $rootScope.$apply(function() {
+        f(snap.val())
+      })   
+    }
+  }
+
+  function ignoreServer(f:Function) {
+    updating = true
+    f()
+    updating = false
+  }
+
+
+
+
+
+
+
+  // Yay, now time for a shared array!
+  function bindArray(ref:fire.IRef):SharedArray {
+    var sa:SharedArray = {ref: ref, value:[]}
+
+    sa.onAdded = makeUpdate(function(value) {
+      sa.value.push(value)
+    })
+
+    sa.onChanged = makeUpdate(function(value) {
+      var obj = _.find(sa.value, byId(value.id))
+      objectReplace(obj, value)
+    })
+
+    sa.onRemoved = makeUpdate(function(value) {
+      var index = indexOf(sa.value, byId(value.id))
+      if (index < 0) return
+      sa.value.splice(index, 1)
+    })
+
+    ref.on('child_added', sa.onAdded)
+    ref.on('child_changed', sa.onChanged)
+    ref.on('child_removed', sa.onRemoved)
+
+    return sa
+  }
+
+  function unbindArray(sa:SharedArray) {
+    sa.ref.off('child_added', sa.onAdded)
+    sa.ref.off('child_changed', sa.onChanged)
+    sa.ref.off('child_removed', sa.onRemoved)
+  }
+
+  function add(arrayRef:fire.IRef, item:SharedArrayItem) {
+    setChild(arrayRef, item)
+  }
+
+  function remove(arrayRef:fire.IRef, item:SharedArrayItem) {
+    arrayRef.child(item.id).remove()
+  }
+
+  // like set, call this if you already know you've updated locally
+  function setChild(arrayRef:fire.IRef, item:SharedArrayItem) {
+    set(arrayRef.child(item.id), item, false)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function byId(id:string):(item:SharedArrayItem) => bool {
+    return function(item:SharedArrayItem) {
+      return (item.id == id)
+    }
+  }
+
+
+  function indexOf(array:any[], iterator:(item:any) => bool):number {
+    for (var i = 0; i < array.length; i++) {
+      if (iterator(array[i])) return i
+    }
+    return -1
+  }
+
+  function objectEmpty(obj:any) {
+    Object.keys(obj).forEach(function(key) {
+      delete obj[key]
+    })
+  }
+
+  function objectReplace(dest:any, source:any) {
+    objectEmpty(dest)
+    objectExtend(dest, source)
+  }
+
+  function objectExtend(dest:any, source:any) {
+    Object.keys(source).forEach(function(key) {
+      dest[key] = source[key]
+    })
   }
 })
 
 
+
 // Data Snapshot: contains all the data at that point in time. Including all child data
 // you could do complicated nested objects with this!
+
+// Object.defineProperty() enumerable defaults to false!
